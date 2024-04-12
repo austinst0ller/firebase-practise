05742f37dd551b9203a9f64b6fbfc1c2a3912cd5
Austin <astoller66@gmail.com>
2024-04-12 12:36:45 -0500

    finshed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { auth } from './firebase/init';
+import { auth, db } from './firebase/init';
+import { collection, addDoc, getDocs, getDoc, doc, query, where, updateDoc, deleteDoc } from "firebase/firestore";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -12,6 +13,59 @@ function App() {
   const [user, setUser] = React.useState({})
   const [loading, setLoading] = React.useState(true)
 
+  async function updatePost() {
+    const hardcodedId = "cIogfv1ZbkL5GoXzAnvo";
+    const postRef = doc(db, "posts", hardcodedId);
+    const post = await getPostById(hardcodedId);
+    const newPost = {
+      /*
+        the spread operator copies all the data from the ID we fetched
+        and then just pass in the only field you want to change.
+        In this instance we only wanted to change the title
+      */
+      ...post,
+      title: "Land a $400k job"
+    };
+    updateDoc(postRef, newPost);
+  }
+
+  function deletePost() {
+    const hardcodedId = "cIogfv1ZbkL5GoXzAnvo";
+    const postRef = doc(db, "posts", hardcodedId);
+    deleteDoc(postRef);
+  }
+
+  function createPost() {
+    const post = {
+      title: "Finish Interview Section",
+      description: "Do Frontend Simplified",
+      uid: user.uid,
+    };
+    addDoc(collection(db, "posts"), post)
+  }
+
+  async function getAllPosts() {
+    const { docs } = await getDocs(collection(db, "posts"));
+    // using data() function from firebase to tranfer to javascript
+    const posts = docs.map((elem) => ({ ...elem.data(), id: elem.id }));
+    console.log(posts)
+  }
+
+  async function getPostById(id) {
+    const postRef = doc(db, "posts", id);
+    const postSnap = await getDoc(postRef)
+    return postSnap.data();
+  }
+
+  async function getPostByUid() {
+    const postCollectionRef = await query(
+      collection(db, "posts"),
+      where("uid", "==", user.uid)
+    );
+    const { docs } = await getDocs(postCollectionRef);
+    console.log(docs.map(doc => doc.data()));
+  }
+
   // if you only want this to run on mount, you HAVE to pass
   // in an empty arr.
   // this allows the user to be auto logged in if they refresh
@@ -20,13 +74,12 @@ function App() {
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setLoading(false)
-      console.log(user)
       if (user) {
         setUser(user)
       }
     })
   }, [])
-  
+
   // to display the first character of the user's email:
   // user.email[0].toUppserCase();
 
@@ -62,6 +115,12 @@ function App() {
       <button onClick={login}>Login</button>
       <button onClick={logout}>Logout</button>
       {loading ? 'loading...' : user.email}
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={getAllPosts}>Get All Posts</button>
+      <button onClick={getPostById}>Get Post By Id</button>
+      <button onClick={getPostByUid}>Get Post By Uid</button>
+      <button onClick={updatePost}>Update Post</button>
+      <button onClick={deletePost}>Delete Post</button>
     </div>
   );
 }
